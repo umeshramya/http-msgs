206ba0f8c8f530c039d6ec547f9241b6dd339bbc
Umesh R Bilagi <umeshbilagi@gmail.com>
2018-03-05 01:05:04 +0530

    cookie

--- src/cookie.js
+++ src/cookie.js
@@ -6,7 +6,7 @@
 
 var queryString = require("querystring");
 
-var setCookie = function(req, res, cookieString, data,  resEnd=false){
+var setCookie = function(req, res, cookieString, data="",  resEnd=false){
     // this sets new cookie 
     res.writeHead(200, {
         'Set-Cookie': cookieString,
